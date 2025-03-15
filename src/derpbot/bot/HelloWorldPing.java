5d45364ccd7ecbb4b280ec58a88a4da4c8bf427e
derp :3 <iid3rp.francismadanlo@gmail.com>
2025-03-15 09:48:31 +0800

    adding some coding blocks for testing

---
+++ src/derpbot/bot/HelloWorldPing.java
@@ -0,0 +1,25 @@
+
+package derpbot.bot;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class HelloWorldPing extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+        String message = event.getMessage().getContentRaw();
+        String[] messages = message.split(" ");
+
+        if (message.equalsIgnoreCase("hello world"))
+            event.getChannel().sendMessage("Hello to you too!").queue();
+
+        if(message.equalsIgnoreCase("nanotime"))
+            event.getChannel().sendMessage("System's Nano time: " + System.nanoTime());
+
+        if(message.equalsIgnoreCase("currenttime"))
+            event.getChannel().sendMessage("Current time: <t:" + System.currentTimeMillis() / 1000 + ">");
+
+    }
+}
---
+++ src/derpbot/bot/SlashCommandExample.java
@@ -0,0 +1,65 @@
+
+package derpbot.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import org.jetbrains.annotations.NotNull;
+
+public class SlashCommandExample extends ListenerAdapter {
+
+    public static void registerCommands(JDA jda) {
+
+
+        CommandListUpdateAction commands = jda.updateCommands();
+
+
+        commands.addCommands(
+                Commands.slash("ping", "Calculate ping of the bot")
+        );
+
+
+        commands.addCommands(
+                Commands.slash("say", "Make the bot say something")
+                        .addOptions(new OptionData(OptionType.STRING, "content", "What the bot should say")
+                                .setRequired(true)) //This command requires a parameter
+        );
+
+
+        commands.addCommands(
+                Commands.slash("add", "Adds two numbers together")
+                        .addOptions(
+                                new OptionData(OptionType.INTEGER, "num1", "The first number").setRequired(true),
+                                new OptionData(OptionType.INTEGER, "num2", "The second number").setRequired(true)
+                        )
+        );
+
+        commands.queue();
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        event.deferReply(false).queue();
+
+        switch (event.getName()) {
+            case "ping":
+                long time = System.currentTimeMillis();
+                event.getHook().sendMessage("Pong!").queue(m -> {
+                    m.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                });
+                break;
+            case "say":
+                event.getHook().sendMessage(event.getOption("content").getAsString()).queue();
+                break;
+            case "add":
+                int num1 = event.getOption("num1").getAsInt();
+                int num2 = event.getOption("num2").getAsInt();
+                event.getHook().sendMessage(String.valueOf(num1 + num2)).queue();
+                break;
+        }
+    }
+}
